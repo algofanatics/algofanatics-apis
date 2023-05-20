ebd324450ab4102126f3756620e5e33205cfa621
Mujeeb Adebowale <olukayodedayo100@gmail.com>
2023-05-20 09:20:22 +0100

    setup auth service and user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module,Logger } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthenticationModule } from './authentication/authentication.module';
 import config from './config'
+import { JwtModule } from '@nestjs/jwt';
+import { UserModule } from './user/user.module';
 
 
 @Module({
@@ -16,7 +19,18 @@ import config from './config'
         uri: configService.get<string>('uri'),
       }),
       inject: [ConfigService],
-    })
+    }),
+    JwtModule.registerAsync({
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('jwt_secret'),
+        signOptions: {
+          expiresIn: configService.get('jwt_expiration_time_in_secs')
+        }
+      }),
+      inject: [ConfigService]
+    }),
+    AuthenticationModule,
+    UserModule
   ],
   controllers: [],
   providers: [],
