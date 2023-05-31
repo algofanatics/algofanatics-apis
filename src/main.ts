31db8dbc183fc341e1d72d2376d641c9cb27a2af
scriptlord <folusolibrary@gmail.com>
2023-05-31 01:48:41 +0100

    feat: add global error and throw others to service

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,13 @@ import { AppModule } from '../app.module';
 import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
 
 const logger: Logger = new Logger('Main');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new GlobalExceptionFilter());
 
   const options = new DocumentBuilder()
     .setTitle('ALGOFANATICS API')
